52a9525e84a9a5f3db286ef18e2b3dc37965cc05
valrakamol <valrakamol.kh.65@ubu.ac.th>
2023-06-30 11:18:28 +0700

    profils-caed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
 
+function App() { 
+  return(
+  <>
+  <div className='card'>
+    <Avatar/>
+    <div className='data'>
+      <Intro/>
+      <SkillList/>
+       </div>
+    </div>
+  </>
+  );
+  }
+  
+function Avatar() {
+  return <img className='Avatar' str='S__12795908.jpg'alt="My Avater" />;
+}
+function SkillList(){
+  return(
+    <div className='skill-list'>
+      <Skill skill='Unity' emoji='🖕'color='yello'/>
+      <Skill skill='Video' emoji='🖕'color='orange'/>
+      <Skill skill='React' emoji='🖕'color='yellow'/>
+      <Skill skill='Myspl' emoji='🖕'color='red'/>
+    </div>
+  );
+}
+function Skill(props){
+  return(
+    <div className='skill' style={{backgroundColor: props.color}}>
+      <span>{props.skill}</span>
+      <span></span>
+    </div>
+  );
+
+  }
+function Intro(){
+  return(
+    <div>
+      <h1>วรกมล คำแสนราช</h1>
+      <p>
+        นักศึกษาคณะวิทยาศาสตร์สาขาเทคโนโลยีสารสนเทศเเละการสื่อสาร
+        มหาวิทยาลัยอุบลราชธานี ชื่นชอบการสร้างคลิปวิดีโอ,วาดรูประบายสี และร้องเพลง
+      </p>
+    </div>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
